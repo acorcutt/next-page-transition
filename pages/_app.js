963d28b05f8098ffe58453eb53a942e2fa439fe5
Anthony Corcutt <acorcutt@gmail.com>
2021-07-12 20:09:59 +0100

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,40 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { useRouter } from 'next/router';
+import { useState, useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [transition, setTransition] = useState({ opacity: 1.0, transform: 'scale(1.0)' });
+  const router = useRouter();
+
+  // Router Events
+  useEffect(() => {
+    const handleRouteChangeStart = (url) => {
+      console.log('handleRouteChangeStart');
+      setTransition({ opacity: 0.0, transform: 'scale(0.5)' });
+    };
+    const handleRouteChangeComplete = (url) => {
+      console.log('handleRouteChangeComplete');
+      // You need to wait a few ms when the load is instant to trigger the css animation
+      // It might be best to time the route change and only delay if instant
+      setTimeout(() => {
+        setTransition({ opacity: 1.0, transform: 'scale(1.0)' });
+      }, 500);
+    };
+
+    router.events.on('routeChangeStart', handleRouteChangeStart);
+    router.events.on('routeChangeComplete', handleRouteChangeComplete);
+
+    return () => {
+      router.events.off('routeChangeStart', handleRouteChangeStart);
+      router.events.off('routeChangeComplete', handleRouteChangeComplete);
+    };
+  }, []);
+
+  return (
+    <div style={{ transition: 'all 1s', ...transition }}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/about.js
+++ pages/about.js
@@ -14,7 +14,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="/">Home</a>
+          Welcome to <a href="/about">About</a>
         </h1>
 
         <p className={styles.description}>Navigate between pages to see page transition.</p>
